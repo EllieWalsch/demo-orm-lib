e84d8b92820bee88f1cbdb2fdef9ff408d3212aa
EllieWalsch <elliewalsch4@gmail.com>
2023-01-09 22:02:58 -0600

    Add classwork

---
+++ app/book/routes.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import controller from "./controller.js";
+
+const router = Router();
+
+router.post("/seed", (_, res) => {
+  controller
+    .seed()
+    .then(() => {
+      res.json({ message: "Seeded books" });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+router.get("/", (_, res) => {
+  controller
+    .findAllBooks()
+    .then((books) => {
+      res.json(books);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+export default router;
--- app/index.js
+++ app/index.js
@@ -1,3 +1,14 @@
-import config from "./config.js";
+import express from "express";
+import bookRouter from "./book/routes.js";
 
-console.log(config);
+const PORT = 3001;
+const app = express();
+
+// middleware to parse incoming request bodies (eg POST)
+app.use(express.json());
+
+app.use("/api/books", bookRouter);
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
